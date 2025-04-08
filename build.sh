b83ebe0bf8a596642dc05b8df4904f844944e3a4
Cornett <cornettjacob@smail.com>
2025-04-08 18:10:15 -0400

    Fix WSGI configuration and paths

--- build.sh
+++ build.sh
@@ -2,11 +2,16 @@
 # exit on error
 set -o errexit
 
+# Move to the Django project directory
+cd djangoApp/gameCatalog
+
 # Upgrade pip first
 /opt/render/project/python/bin/python -m pip install --upgrade pip
 
 # Then install requirements
-pip install -r requirements.txt
+pip install -r ../../requirements.txt
 
 # Run Django commands
 python manage.py collectstatic --noinput
+
+
